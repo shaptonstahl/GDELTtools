096b6e08672b6fa2e24ff94ef8e4ef9d8b6fa5d1
Stephen Haptonstahl <srh@haptonstahl.org>
2013-10-01 18:42:50 -0400

    v1.0 ready for CRAN

--- Rpackage/dev/GDELTtools/R/NormEventCounts.R
+++ Rpackage/dev/GDELTtools/R/NormEventCounts.R
@@ -3,16 +3,14 @@
 #' Scale event counts based on the unit of analysis.
 #' 
 #' @aliases NormEventCounts
-#' @param data data.frame, a GDELT dataframe.
-#' @param include.events character, vector with CAMEO event codes to include in the count.
-#' @param condition list, any additional conditions for coding the event count, such as, e.g., restricting to particular actor codes. this needs to refer to dataframe declared above.
+#' @param x data.frame, a GDELT data.frame.
 #' @param unit.analysis character, default is country.day; other options: country.day, country.month, country.year, day, month, year
-#' @param var.name, character, base name for the new count variables
+#' @param var.name character, base name for the new count variables
 #' @return data.frame
 #' @export
 #' @details
-#' For \code{unit.analysis}, day and country-day put out a dataset where date is of class \sQuote{date}.
-#  All other options put out a dataset where year or month is integer (this needs to be unified in a later version).
+#' For \code{unit.analysis}, day and country-day put out a data set where date is of class \sQuote{date}.
+#  All other options put out a data set where year or month is integer (this needs to be unified in a later version).
 #' @references
 #' GDELT: Global Data on Events, Location and Tone, 1979-2012.  
 #' Presented at the 2013 meeting of the International Studies Association
@@ -25,114 +23,115 @@
 #' }
 #' @examples
 #' \dontrun{
-#' new.data.frame <- NormEventCounts(data = GDELT.subset.data, 
-#'   include.events = c(141, 145, 140, 143, 142, 144, 1412, 1413, 1411, 1431, 1414), 
-#'   condition = (GDELT.subset.data $ActionGeo_CountryCode == "AF"), 
-#'   unit.analysis = "country.year", 
-#'   var.name = "protest")}
-NormEventCounts <- function(data, 
-                            include.events, 
-                            condition, 
+#' GDELT.subset.data <- GetGDELT("2012-01-01", "2012-12-31", allow.wildcards=TRUE,
+#'                               filter=list(ActionGeo_CountryCode=c("AF", "US"), EventCode="14*"))
+#' GDELT.normed.data <- NormEventCounts(x = GDELT.subset.data, 
+#'                                      unit.analysis="country.year", 
+#'                                      var.name="protest")}
+NormEventCounts <- function(x, 
                             unit.analysis, 
                             var.name){
   # files with total event counts are included in the package, but these could be stored on a server and downloaded as needed 
-  if(missing(data)) stop("No dataframe!")
-  if(missing(include.events)) stop("You must declare event codes to subset on!")
-  if(missing(unit.analysis)) unit.analysis <- "country.day" # default
-  if(!(unit.analysis %in% c("country.day", "country.month", "country.year", "day", "month", "year"))) stop("Not a valid unit of analysis! \nChoose from: country.day, country.month, country.year, day, month, year")
-  if(missing(condition)) condition <- TRUE # no further restrictions on the dataset
+  if(missing(x)) stop("Must specify a data.frame")
+  if(missing(unit.analysis)) stop("A unit of analysis must be specified")
+  if(!(unit.analysis %in% c("country.day", "country.month", "country.year", "day", "month", "year"))) stop("Not a valid unit of analysis. Choose from: country.day, country.month, country.year, day, month, year")
   
-  data <- subset(data, subset = (condition & EventCode %in% include.events)) # keep only desired events and impose restrictions
+  # load normalization data
+  # THIS IS A KLUDGE THAT SHOULD BE FIXED
+  e <- new.env()
+  data(NormEventCountsData, envir=e)
+  NormEventCountsData <- get("NormEventCountsData", envir=e)
+  rm(e)
   
-  if(unit.analysis == "country.day"){ # this is the default (see here for code annotations)
-    data$count <- tapply(data$EventCode, paste(data$ActionGeo_CountryCode, data$SQLDATE), length)[paste(data$ActionGeo_CountryCode, data$SQLDATE)] # code event counts
-    load(unz(paste(getwd(), "/RData.zip", sep = ""), "daily_country.RData")) # get total counts 
-    data <- merge(data, daily.country, by.x = c("ActionGeo_CountryCode", "SQLDATE"), by.y = c("country", "day"), all.x = TRUE) # merge the two together
-    data$norm.count <- data$count/data$total # code the normalized count
-    range <- as.Date(as.character(range(data$SQLDATE)), format = "%Y%m%d") # get the first and last date in the data
+  if(unit.analysis == "country.day"){ # see here for code annotations
+    x$count <- tapply(x$EventCode, paste(x$ActionGeo_CountryCode, x$SQLDATE), length)[paste(x$ActionGeo_CountryCode, x$SQLDATE)] # code event counts
+    #load(unz(paste(getwd(), "/RData.zip", sep = ""), "daily_country.RData")) # get total counts 
+    x <- merge(x, NormEventCountsData$daily.country, by.x = c("ActionGeo_CountryCode", "SQLDATE"), by.y = c("country", "day"), all.x = TRUE) # merge the two together
+    x$norm.count <- x$count/x$total # code the normalized count
+    range <- as.Date(as.character(range(x$SQLDATE)), format = "%Y%m%d") # get the first and last date in the data
     days <- format(seq(as.Date(range[1]), to = as.Date(range[2]), by = "1 day"), "%Y%m%d") # create a vector with all dates in the range
-    data <- unique(subset(data, select = c(ActionGeo_CountryCode, SQLDATE, count, norm.count))) # only keep the columns needed & subset to unique country-days
-    load(unz(paste(getwd(), "/RData.zip", sep = ""), "country_codes.RData")) # get the country FIPS 10-4 codes for merging
-    complete <- expand.grid(country = countries$fips104, date = days) # create complete time-series per country
+    x <- unique(subset(x, select = c("ActionGeo_CountryCode", "SQLDATE", "count", "norm.count"))) # only keep the columns needed & subset to unique country-days
+    #load(unz(paste(getwd(), "/RData.zip", sep = ""), "country_codes.RData")) # get the country FIPS 10-4 codes for merging
+    complete <- expand.grid(country = NormEventCountsData$countries$fips104, date = days) # create complete time-series per country
     new.vars <- c(paste(var.name, ".count", sep = ""), paste(var.name, ".norm", sep = "")) # create new names based on user input
-    names(data) <- c("country", "date", new.vars) # assign column names
-    data <- merge(complete, data, by = c("country", "date"), all.x = TRUE) # merge data into complete time-series
-    data[is.na(data[, paste(var.name, ".norm", sep = "")]), new.vars] <- c(0, 0) # code NAs as 0s
-    data[, "date"] <- as.Date(data[, "date"], "%Y%m%d") # coerce into date format
+    names(x) <- c("country", "date", new.vars) # assign column names
+    x <- merge(complete, x, by = c("country", "date"), all.x = TRUE) # merge data into complete time-series
+    x[is.na(x[, paste(var.name, ".norm", sep = "")]), new.vars] <- c(0, 0) # code NAs as 0s
+    x[, "date"] <- as.Date(x[, "date"], "%Y%m%d") # coerce into date format
   } 
   if(unit.analysis == "country.month"){
-    data$count <- tapply(data$EventCode, paste(data$ActionGeo_CountryCode, data$MonthYear), length)[paste(data$ActionGeo_CountryCode, data$MonthYear)]
-    load(unz(paste(getwd(), "/RData.zip", sep = ""), "monthly_country.RData"))
-    data <- merge(data, monthly.country, by.x = c("ActionGeo_CountryCode", "MonthYear"), by.y = c("country", "month"), all.x = TRUE)
-    data$norm.count <- data$count/data$total
-    range <- as.Date(as.character(range(data$SQLDATE)), format = "%Y%m%d") # get the first and last date in the data
+    x$count <- tapply(x$EventCode, paste(x$ActionGeo_CountryCode, x$MonthYear), length)[paste(x$ActionGeo_CountryCode, x$MonthYear)]
+    #load(unz(paste(getwd(), "/RData.zip", sep = ""), "monthly_country.RData"))
+    x <- merge(x, NormEventCountsData$monthly.country, by.x = c("ActionGeo_CountryCode", "MonthYear"), by.y = c("country", "month"), all.x = TRUE)
+    x$norm.count <- x$count/x$total
+    range <- as.Date(as.character(range(x$SQLDATE)), format = "%Y%m%d") # get the first and last date in the data
     months <- as.integer(format(seq(as.Date(range[1]), to = as.Date(range[2]), by = "1 month"), "%Y%m"))
-    data <- unique(subset(data, select = c(ActionGeo_CountryCode, MonthYear, count, norm.count)))
-    load(unz(paste(getwd(), "/RData.zip", sep = ""), "country_codes.RData"))
-    complete <- expand.grid(country = countries$fips104, month = months) 
+    x <- unique(subset(x, select = c("ActionGeo_CountryCode", "MonthYear", "count", "norm.count")))
+    #load(unz(paste(getwd(), "/RData.zip", sep = ""), "country_codes.RData"))
+    complete <- expand.grid(country = NormEventCountsData$countries$fips104, month = months) 
     new.vars <- c(paste(var.name, ".count", sep = ""), paste(var.name, ".norm", sep = ""))
-    names(data) <- c("country", "month", new.vars) 
-    data <- merge(complete, data, by = c("country", "month"), all.x = TRUE)
-    data[is.na(data[, paste(var.name, ".norm", sep = "")]), new.vars] <- c(0, 0)
+    names(x) <- c("country", "month", new.vars) 
+    x <- merge(complete, x, by = c("country", "month"), all.x = TRUE)
+    x[is.na(x[, paste(var.name, ".norm", sep = "")]), new.vars] <- c(0, 0)
   } 
   if(unit.analysis == "country.year"){ 
-    data$count <- tapply(data$EventCode, paste(data$ActionGeo_CountryCode, data$Year), length)[paste(data$ActionGeo_CountryCode, data$Year)]
-    load(unz(paste(getwd(), "/RData.zip", sep = ""), "yearly_country.RData"))
-    data <- merge(data, yearly.country, by.x = c("ActionGeo_CountryCode", "Year"), by.y = c("country", "year"), all.x = TRUE)
-    data$norm.count <- data$count/data$total
-    range <- range(data$Year)
+    x$count <- tapply(x$EventCode, paste(x$ActionGeo_CountryCode, x$Year), length)[paste(x$ActionGeo_CountryCode, x$Year)]
+    #load(unz(paste(getwd(), "/RData.zip", sep = ""), "yearly_country.RData"))
+    x <- merge(x, NormEventCountsData$yearly.country, by.x = c("ActionGeo_CountryCode", "Year"), by.y = c("country", "year"), all.x = TRUE)
+    x$norm.count <- x$count/x$total
+    range <- range(x$Year)
     if(range[2] > 2012) cat("Normalized counts only available until 2012!")
-    data <- unique(subset(data, select = c(ActionGeo_CountryCode, Year, count, norm.count)))
-    load(unz(paste(getwd(), "/RData.zip", sep = ""), "country_codes.RData"))
-    complete <- expand.grid(country = countries$fips104, year = range[1]:range[2])
+    x <- unique(subset(x, select = c("ActionGeo_CountryCode", "Year", "count", "norm.count")))
+    #load(unz(paste(getwd(), "/RData.zip", sep = ""), "country_codes.RData"))
+    complete <- expand.grid(country = NormEventCountsData$countries$fips104, year = range[1]:range[2])
     new.vars <- c(paste(var.name, ".count", sep = ""), paste(var.name, ".norm", sep = ""))
-    names(data) <- c("country", "year", new.vars)
-    data <- merge(complete, data, by = c("country", "year"), all.x = TRUE) 
-    data[is.na(data[, paste(var.name, ".norm", sep = "")]), new.vars] <- c(0, 0)
+    names(x) <- c("country", "year", new.vars)
+    x <- merge(complete, x, by = c("country", "year"), all.x = TRUE) 
+    x[is.na(x[, paste(var.name, ".norm", sep = "")]), new.vars] <- c(0, 0)
   } 
   if(unit.analysis == "day"){
-    data$count <- tapply(data$EventCode, data$SQLDATE, length)[as.factor(data$SQLDATE)]
-    load(unz(paste(getwd(), "/RData.zip", sep = ""), "daily.RData"))
-    data <- merge(data, daily, by.x = "SQLDATE", by.y = "day", all.x = TRUE)		
-    data$norm.count <- data$count/data$total
-    range <- as.Date(as.character(range(data$SQLDATE)), format = "%Y%m%d")
+    x$count <- tapply(x$EventCode, x$SQLDATE, length)[as.factor(x$SQLDATE)]
+    #load(unz(paste(getwd(), "/RData.zip", sep = ""), "daily.RData"))
+    x <- merge(x, NormEventCountsData$daily, by.x = "SQLDATE", by.y = "day", all.x = TRUE)		
+    x$norm.count <- x$count/x$total
+    range <- as.Date(as.character(range(x$SQLDATE)), format = "%Y%m%d")
     days <- format(seq(as.Date(range[1]), to = as.Date(range[2]), by = "1 day"), "%Y%m%d")
-    data <- unique(subset(data, select = c(SQLDATE, count, norm.count)))
+    x <- unique(subset(x, select = c("SQLDATE", "count", "norm.count")))
     complete <- expand.grid(date = days) 
     new.vars <- c(paste(var.name, ".count", sep = ""), paste(var.name, ".norm", sep = ""))
-    names(data) <- c("date", new.vars)
-    data <- merge(complete, data, by = "date", all.x = TRUE)
-    data[is.na(data[, paste(var.name, ".norm", sep = "")]), new.vars] <- c(0, 0)
-    data[, "date"] <- as.Date(data[, "date"], "%Y%m%d")
+    names(x) <- c("date", new.vars)
+    x <- merge(complete, x, by = "date", all.x = TRUE)
+    x[is.na(x[, paste(var.name, ".norm", sep = "")]), new.vars] <- c(0, 0)
+    x[, "date"] <- as.Date(x[, "date"], "%Y%m%d")
   } 
   if(unit.analysis == "month"){
-    data$count <- tapply(data$EventCode, data$SQLDATE, length)[as.factor(data$SQLDATE)]
-    load(unz(paste(getwd(), "/RData.zip", sep = ""), "monthly.RData"))
-    data <- merge(data, monthly, by.x = "MonthYear", by.y = "month", all.x = TRUE)		
-    data$norm.count <- data$count/data$total
-    range <- as.Date(as.character(range(data$SQLDATE)), format = "%Y%m%d")
+    x$count <- tapply(x$EventCode, x$SQLDATE, length)[as.factor(x$SQLDATE)]
+    #load(unz(paste(getwd(), "/RData.zip", sep = ""), "monthly.RData"))
+    x <- merge(x, NormEventCountsData$monthly, by.x = "MonthYear", by.y = "month", all.x = TRUE)		
+    x$norm.count <- x$count/x$total
+    range <- as.Date(as.character(range(x$SQLDATE)), format = "%Y%m%d")
     months <- as.integer(format(seq(as.Date(range[1]), to = as.Date(range[2]), by = "1 month"), "%Y%m"))
-    data <- unique(subset(data, select = c(MonthYear, count, norm.count)))
+    x <- unique(subset(x, select = c("MonthYear", "count", "norm.count")))
     complete <- expand.grid(month = months) 
     new.vars <- c(paste(var.name, ".count", sep = ""), paste(var.name, ".norm", sep = ""))
-    names(data) <- c("month", new.vars)
-    data <- merge(complete, data, by = "month", all.x = TRUE)
-    data[is.na(data[, paste(var.name, ".norm", sep = "")]), new.vars] <- c(0, 0)
+    names(x) <- c("month", new.vars)
+    x <- merge(complete, x, by = "month", all.x = TRUE)
+    x[is.na(x[, paste(var.name, ".norm", sep = "")]), new.vars] <- c(0, 0)
   } 
   if(unit.analysis == "year"){
-    data$count <- tapply(data$EventCode, data$Year, length)[as.factor(data$Year)]
-    load(unz(paste(getwd(), "/RData.zip", sep = ""), "yearly.RData"))
-    data <- merge(data, yearly, by.x = "Year", by.y = "year", all.x = TRUE)		
-    data$norm.count <- data$count/data$total
-    range <- range(data$Year)
+    x$count <- tapply(x$EventCode, x$Year, length)[as.factor(x$Year)]
+    #load(unz(paste(getwd(), "/RData.zip", sep = ""), "yearly.RData"))
+    x <- merge(x, NormEventCountsData$yearly, by.x = "Year", by.y = "year", all.x = TRUE)		
+    x$norm.count <- x$count/x$total
+    range <- range(x$Year)
     if(range[2] > 2012) cat("Normalized counts only available until 2012!")
-    data <- unique(subset(data, select = c(Year, count, norm.count)))
+    x <- unique(subset(x, select = c("Year", "count", "norm.count")))
     complete <- expand.grid(year = range[1]:range[2])
     new.vars <- c(paste(var.name, ".count", sep = ""), paste(var.name, ".norm", sep = ""))
-    names(data) <- c("year", new.vars)
-    data <- merge(complete, data, by = "year", all.x = TRUE)
-    data[is.na(data[, paste(var.name, ".norm", sep = "")]), new.vars] <- c(0, 0)
+    names(x) <- c("year", new.vars)
+    x <- merge(complete, x, by = "year", all.x = TRUE)
+    x[is.na(x[, paste(var.name, ".norm", sep = "")]), new.vars] <- c(0, 0)
   }
-  row.names(data) <- 1:nrow(data)
-  return(data)
+  row.names(x) <- 1:nrow(x)
+  return(x)
 }
